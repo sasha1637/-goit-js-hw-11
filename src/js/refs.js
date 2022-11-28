95274a733032db49fdd1da0f8e4ea1583b0bdb9e
sasha1637 <82914142+sasha1637@users.noreply.github.com>
2022-11-28 14:29:38 +0200

    Update refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,4 @@
-  refs = {
+  const refs = {
     gallery: document.querySelector(".gallery"),
     searchInput: document.querySelector(".search-form"),
     target:document.querySelector('.listItem')
