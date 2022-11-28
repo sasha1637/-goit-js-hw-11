94410611f4dea8ed38b71fec15310efad8b16da2
sasha1637 <82914142+sasha1637@users.noreply.github.com>
2022-11-28 15:47:17 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ let page = 1;
 let searchQuery = ''
 const options = {
   root: null,
-  rootMargin: '1000px',
+  rootMargin: '500px',
   threshold: 1
 }
 const simpleLightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, showCounter: false, captionsData: 'alt' })
@@ -18,25 +18,21 @@ refs.searchInput.addEventListener("submit", onSearchQuery);
 
 function onSearchQuery(evt) {
   evt.preventDefault();
+   refs.gallery.innerHTML=''
   searchQuery = evt.currentTarget.elements.searchQuery.value
 
   imageApi(searchQuery, page).then(
     data => {
       if (!data.hits.length) {
-        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+        
+   return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+         
       }
       refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits))
       simpleLightbox.refresh()
       Notiflix.Notify.info(`"Hooray! We found ${data.totalHits} images."`)
       observer.observe(refs.target);
-const { height: cardHeight } = document
-  .querySelector(".gallery")
-  .firstElementChild.getBoundingClientRect();
 
-window.scrollBy({
-  top: cardHeight * 2,
-  behavior: "smooth",
-});
     }
   ).catch(err => console.log(err))
 
@@ -48,21 +44,24 @@ function onLoad(entries, observer) {
       page += 1;
       imageApi(searchQuery, page).then(data => {
         refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits))
+        scrollBy()
         simpleLightbox.refresh()
         if (page >= (data.totalHits / 40)) {
           observer.unobserve(refs.target)
+          Notiflix.Notify.info('Для завантаження більше немає зображень')
         }
       })
     }
   })
 }
 
+function scrollBy() {
+  const { height: cardHeight } = refs.gallery
+    .firstElementChild.getBoundingClientRect();
 
-// const { height: cardHeight } = document
-//   .querySelector(".gallery")
-//   .firstElementChild.getBoundingClientRect();
+window.scrollBy({
+  top: cardHeight ,
+  behavior: "smooth",
+});
+}
 
-// window.scrollBy({
-//   top: cardHeight * 102,
-//   behavior: "smooth",
-// });
